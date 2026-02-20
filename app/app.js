90ca2aefe5087b0dcbe4957de44230f8f17ac797
sohan0911 <167161466+sohan0911@users.noreply.github.com>
2026-02-20 11:43:26 +0000

    pug template test

--- app/app.js
+++ app/app.js
@@ -7,12 +7,16 @@ var app = express();
 // Add static files location
 app.use(express.static("static"));
 
+// Use the Pug templating engine
+app.set('view engine', 'pug');
+app.set('views', './app/views');
+
 // Get the functions in the db.js file to use
 const db = require('./services/db');
 
-// Create a route for root - /
+// Create a route for root
 app.get("/", function(req, res) {
-    res.send("Hello world!");
+    res.render("index");
 });
 
 // Create a route for testing the db
